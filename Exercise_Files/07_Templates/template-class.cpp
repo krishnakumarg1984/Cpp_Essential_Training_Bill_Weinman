465b50796e47a1199927dc0c4ae9103ee534ad35
Krishnakumar <krishna.kumar@ucl.ac.uk>
2019-07-02 16:35:10 +0100

    didn't quite understand template-class

--- Exercise_Files/07_Templates/template-class.cpp
+++ Exercise_Files/07_Templates/template-class.cpp
@@ -9,7 +9,7 @@ using namespace std;
 class E : public std::exception {
     const char * msg;
     E(){};    // no default constructor
-public:
+ public:
     explicit E(const char * s) throw() : msg(s) { }
     const char * what() const throw() { return msg; }
 };
@@ -17,16 +17,16 @@ public:
 // simple fixed-size LIFO stack template
 template <typename T>
 class bwstack {
-private:
+ private:
     static const int defaultsize = 10;
     static const int maxsize = 1000;
     int _size;
     int _top;
     T * _stkptr;
-public:
+ public:
     explicit bwstack(int s = defaultsize);
     ~bwstack() { delete[] _stkptr; }
-    T & push( const T & );
+    T & push(const T &);
     T & pop();
     bool isempty() const { return _top < 0; }
     bool isfull() const { return _top >= _size - 1; }
@@ -36,65 +36,67 @@ public:
 
 // Stack<T> constructor
 template <typename T>
-bwstack<T>::bwstack ( int s ) {
-    if(s > maxsize || s < 1) throw E("invalid stack size");
-    else _size = s;
+bwstack<T>::bwstack(int s) {
+    if (s > maxsize || s < 1)
+        throw E("invalid stack size");
+    else
+        _size = s;
     _stkptr = new T[_size];
     _top = -1;
 }
 
 template <typename T>
-T & bwstack<T>::push ( const T & i ) {
-    if(isfull()) throw E("stack full");
+T & bwstack<T>::push(const T & i) {
+    if (isfull()) throw E("stack full");
     return _stkptr[++_top] = i;
 }
 
 template <typename T>
-T & bwstack<T>::pop () {
-    if(isempty()) throw E("stack empty");
+T & bwstack<T>::pop() {
+    if (isempty()) throw E("stack empty");
     return _stkptr[_top--];
 }
 
-int main( int argc, char ** argv ) {
+int main(int argc, char ** argv) {
     try {
         bwstack<int> si(5);
-        
+
         cout << "si size: " << si.size() << endl;
         cout << "si top: " << si.top() << endl;
-        
+
         for ( int i : { 1, 2, 3, 4, 5 } ) {
             si.push(i);
         }
-        
+
         cout << "si top after pushes: " << si.top() << endl;
         cout << "si is " << ( si.isfull() ? "" : "not " ) << "full" << endl;
-        
-        while(!si.isempty()) {
+
+        while (!si.isempty()) {
             cout << "popped " << si.pop() << endl;
         }
     } catch (E & e) {
         cout << "Stack error: " << e.what() << endl;
     }
-    
+
     try {
         bwstack<string> ss(5);
-        
+
         cout << "ss size: " << ss.size() << endl;
         cout << "ss top: " << ss.top() << endl;
-        
+
         for ( const char * s : { "one", "two", "three", "four", "five" } ) {
             ss.push(s);
         }
-        
+
         cout << "ss top after pushes: " << ss.top() << endl;
-        cout << "ss is " << ( ss.isfull() ? "" : "not " ) << "full" << endl;
-        
-        while(!ss.isempty()) {
+        cout << "ss is " << (ss.isfull() ? "" : "not ") << "full" << endl;
+
+        while (!ss.isempty()) {
             cout << "popped " << ss.pop() << endl;
         }
     } catch (E & e) {
         cout << "Stack error: " << e.what() << endl;
     }
-    
+
     return 0;
 }
--- Exercise_Files/07_Templates/template-function.cpp
+++ Exercise_Files/07_Templates/template-function.cpp
@@ -1,15 +1,21 @@
 // template-function.cpp by Bill Weinman <http://bw.org/>
 // updated 2018-08-18
 #include <cstdio>
+#include <string>
 using namespace std;
 
-template <typename T>
-T maxof ( T a, T b ) {
+template <typename T>  // typename is interchangeable with 'class' here
+//note: templates cannot be defined within a block
+T maxof(T a, T b) {
     return ( a > b ? a : b );
 }
 
-int main( int argc, char ** argv ) {
-    int m = maxof<int>( 7, 9 );
-    printf("max is: %d\n", m);
+int main(int argc, char ** argv) {
+    // int m = maxof<int>(7, 9);
+    // printf("max is: %d\n", m);
+    // string m = maxof<string>("seven", "nine");
+    // string m = maxof<string>("nine", "seven");
+    string m = maxof<string>("seven", "nine");
+    printf("max is: %s\n", m.c_str());
     return 0;
 }
